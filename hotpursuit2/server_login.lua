11fa217cce4553bdcebf36accbc32bab5c31b5c4
nynjardin <nynjardin@gmail.com>
2017-01-16 11:36:24 +1100

    Delete server_login.lua

--- hotpursuit2/server_login.lua
+++
@@ -1,6 +0,0 @@
-
--- Custom event gets called when a client joins
-RegisterServerEvent('foundation:onPlayerConnect')
-AddEventHandler('foundation:onPlayerConnect', function()
-	TriggerClientEvent('foundation:playerLog', source, true)
-end)
